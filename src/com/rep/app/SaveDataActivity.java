2490e2f04a9f353cc5bb00b61cfcb4a66e29d160
renjie120 <lishuiqing110@163.com>
2014-05-18 18:55:28 +0800

    添加时间选择控件

--- src/com/rep/app/SaveDataActivity.java
+++ src/com/rep/app/SaveDataActivity.java
@@ -5,10 +5,17 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import kankan.wheel.widget.ScreenInfo;
+import kankan.wheel.widget.WheelMain;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.rep.util.ActionBar;
@@ -22,10 +29,50 @@ import com.rep.util.ActionBar.Action;
  */
 public class SaveDataActivity extends BaseActivity {
 	private ActionBar head;
+	private LinearLayout indateBtn;
 	private TextView comeInNum, intreNum, tryNum, buyNum, oldNum, timeSpan,
 			indate, indate_day;
 	private static final String[] weeks = new String[] { "星期日", "星期一", "星期二",
 			"星期三", "星期四", "星期五", "星期六" };
+	private WheelMain wheelMain;
+	private AlertDialog dialog;
+
+	/***
+	 * 
+	 * 时间滚动器
+	 * 
+	 */
+	public void showDateTimePicker() {
+		LayoutInflater inflater = LayoutInflater.from(SaveDataActivity.this);
+		View timepickerview = inflater.inflate(R.layout.selectbirthday, null);
+		timepickerview.setMinimumWidth(getWindowManager().getDefaultDisplay()
+				.getWidth());
+		ScreenInfo screenInfo = new ScreenInfo(SaveDataActivity.this);
+		wheelMain = new WheelMain(timepickerview);
+		wheelMain.screenheight = screenInfo.getHeight();
+		Calendar calendar = Calendar.getInstance();
+		// calendar.setTime(dateFormat.parse(time));设置指定时间
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		wheelMain.setTime(year, month, day);
+		dialog = new AlertDialog.Builder(this).setView(timepickerview).show();
+
+//		Window window = dialog.getWindow();
+//		window.setGravity(Gravity.BOTTOM); // 此处可以设置dialog显示的位置
+//		window.setWindowAnimations(R.style.mystyle); // 添加动画
+
+		Button btn = (Button) timepickerview
+				.findViewById(R.id.btn_datetime_sure);
+		btn.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				 
+				dialog.dismiss();
+			}
+		});
+
+	}
 
 	/**
 	 * 界面初始化函数.
@@ -50,7 +97,7 @@ public class SaveDataActivity extends BaseActivity {
 		GregorianCalendar ca = new GregorianCalendar();
 		ca.setTime(date);
 		int i = ca.get(Calendar.DAY_OF_WEEK);
-		return weeks[i-1];
+		return weeks[i - 1];
 	}
 
 	/**
@@ -84,6 +131,7 @@ public class SaveDataActivity extends BaseActivity {
 				startActivity(intent2);
 			}
 		});
+		indateBtn =  (LinearLayout) findViewById(R.id.indateBtn);
 		comeInNum = (TextView) findViewById(R.id.comein_v);
 		intreNum = (TextView) findViewById(R.id.intre_v);
 		tryNum = (TextView) findViewById(R.id.try_v);
@@ -95,5 +143,13 @@ public class SaveDataActivity extends BaseActivity {
 		Date t = new Date();
 		indate.setText(toString(t));
 		indate_day.setText(getDayOfWeek(t));
+		
+		indateBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showDateTimePicker();
+			}
+		});
 	}
 }
